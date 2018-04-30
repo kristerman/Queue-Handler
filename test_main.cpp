4a2815e5d6b26eb581e6377809a5acb89d0a846a
Christos Karamitsos <kris.karamits@gmail.com>
2018-05-01 00:31:41 +0100

    Lock mutex when using cout

--- test_main.cpp
+++ test_main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <mutex>
 #include "work_item.hpp"
 #include "queue_handler.hpp"
 using namespace std;
 
+static mutex mtx;
+
 void read_items(int mode, QueueHandler<shared_ptr<WorkItem> > &q) {
   if (mode == 0) {
-      cout << "using pop" << endl;
       while (true) {
           auto work_itm = q.pop();
+          mtx.lock();
           cout << "Reading: " << *work_itm << endl;
+          mtx.unlock();
       }
   } else {
       while (true) {
           try{
               auto work_itm = q.pop_try();
+              mtx.lock();
               cout << "Reading: " << *work_itm << endl;
+              mtx.unlock();
           } catch (int e) {
+              mtx.lock();
               cout << "No item in queue, exiting thread" << endl;
+              mtx.unlock();
               break;
           }
       }
@@ -30,11 +38,15 @@ void insert_items(QueueHandler<shared_ptr<WorkItem> > &q) {
   while (true) {
       int type = rand() % 3;
       shared_ptr<WorkItem> sp(new WorkItem(type));
+      mtx.lock();
       cout << "Inserting " << *sp << endl;
+      mtx.unlock();
       try {
           q.push(sp);
       } catch (int e) {
+          mtx.lock();
           cout << "Error: could not push item" << endl;
+          mtx.unlock();
           break;
       }
       // Add sleep to simulate a writer thread that pushes on events
